9b7401d00d891b6b25490980ea4a11977bb444e3
Tymofii Baga <tim.baga@here.com>
2018-01-23 14:33:09 +0100

    Example subscriber posts stats about data received, server has better motor config loading

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import time
 import yaml
 from lidar import PubSocket
 from lidar.scanner import TranslateReadings
@@ -11,14 +12,17 @@ with open("./config.yaml", "rt") as f:
 print("Opening pub socket")
 
 pub = PubSocket(cfg)
+initial_motor_speed = cfg.get('motor_speed', 1)
+initial_sample_rate = cfg.get('sample_rate', 500)
+
 
 with Sweep(cfg['tty']) as sweep:
-    print("Setting the motor settings up")
-    sweep.set_motor_speed(cfg.get('motor_speed', 2))
-    sweep.set_sample_rate(cfg.get('scaning_frequency', 500))
+    sweep.set_motor_speed(initial_motor_speed)
+    sweep.set_set_sample_rate(initial_sample_rate)
     while not sweep.get_motor_ready():
-        print("Waiting for a sweep motor to become ready...")
-        time.sleep(2)
+        print("Waiting for a lidar motor to become ready")
+        time.sleep(1)
+
     sweep.start_scanning()
     print("All set, liftoff")
 
--- sub.py
+++ sub.py
@@ -1,5 +1,6 @@
 import yaml
 from lidar import SubSocket
+import numpy as np
 
 with open("./config.yaml", "rt") as f:
     cfg = yaml.load(f)
@@ -9,5 +10,10 @@ print("Opening sub socket")
 socket = SubSocket(cfg)
 
 print("Receiving data")
+
 for payload in socket:
     print("Received payload of {s} measurements taken at {t}".format(s=len(payload['scan']), t=payload['time']))
+    scan = payload['scan']
+    pts = np.array([(x['distance'], x['angle']) for x in scan])
+    print(f"Median distance: {np.median(pts[:, 0]):.4f} (min {np.min(pts[:, 0]):.4f}, max: {np.max(pts[:, 0]):.4f}, +- {np.std(pts[:, 0]):.4f}")
+    print(f"Median angle: {np.median(pts[:, 1]):.4f} (min {np.min(pts[:, 1]):.4f}, max: {np.max(pts[:, 1]):.4f}, +- {np.std(pts[:, 1]):.4f}")
