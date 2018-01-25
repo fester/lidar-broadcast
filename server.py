6bbf9c742c0eef7fa76822cd3f58b59357a65037
Tymofii Baga <tim.baga@here.com>
2018-01-25 15:17:21 +0100

    Added SIGINT handler in order to gracefully shut the lidar down.

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
+import sys
+import signal
 import time
 import yaml
+from functools import partial
+
 from lidar import PubSocket
 from lidar.scanner import TranslateReadings
 import time
@@ -11,12 +15,23 @@ with open("./config.yaml", "rt") as f:
 
 print("Opening pub socket")
 
+
+def sigint_handler(sweep, sig, frame):
+    print("Lidar shut down sequence initialized")
+    sweep.stop_scanning()
+    sweep.set_motor_speed(0)
+    print("Lidar shut down sequence finished")
+    sys.exit(0)
+    
+
 pub = PubSocket(cfg)
 initial_motor_speed = cfg.get('motor_speed', 1)
 initial_sample_rate = cfg.get('sample_rate', 500)
 print("Initial speed: {}, sampling rate: {}".format(initial_motor_speed, initial_sample_rate))
 
 with Sweep(cfg['tty']) as sweep:
+    signal.signal(signal.SIGINT, partial(sigint_handler, sweep))
+
     sweep.set_motor_speed(initial_motor_speed)
     sweep.set_sample_rate(initial_sample_rate)
     while not sweep.get_motor_ready():
