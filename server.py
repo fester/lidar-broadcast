188661c38960504da02be5bb9802f1e7fd2bfc33
Tim Baga <tim.baga@here.com>
2018-01-25 13:16:07 +0100

    A bit more info on motor startup for server

--- server.py
+++ server.py
@@ -14,14 +14,16 @@ print("Opening pub socket")
 pub = PubSocket(cfg)
 initial_motor_speed = cfg.get('motor_speed', 1)
 initial_sample_rate = cfg.get('sample_rate', 500)
-
+print("Initial speed: {}, sampling rate: {}".format(initial_motor_speed, initial_sample_rate))
 
 with Sweep(cfg['tty']) as sweep:
     sweep.set_motor_speed(initial_motor_speed)
-    sweep.set_set_sample_rate(initial_sample_rate)
+    sweep.set_sample_rate(initial_sample_rate)
     while not sweep.get_motor_ready():
         print("Waiting for a lidar motor to become ready")
         time.sleep(1)
+
+    print("Actual motor speed: {}, sampling rate: {}".format(sweep.get_motor_speed(), sweep.get_sample_rate()))
 
     sweep.start_scanning()
     print("All set, liftoff")
