b26348565899e28595e2b06aee8be827104a9e99
Tymofii Baga <tim.baga@here.com>
2018-02-01 20:20:31 +0100

    Suddenly stable working A2 lidar and breezyslam. Remember this moment.

--- breezy.py
+++ breezy.py
@@ -1,3 +1,5 @@
+from threading import Thread
+import time
 import sys
 import yaml
 import math
@@ -16,6 +18,8 @@ MAP_SIZE_PIXELS = 3000
 MAP_SIZE_METERS = 30
 SCREEN_SIZE_PIXELS = 900
 
+g_Slam = {'core': None, 'scan': [], 'map': None, 'pos': (0, 0, 0)}
+g_Quitting = False
 
 class ScanseLidar(Laser):
     ENGINE_ROTATION_HZ = 6
@@ -37,6 +41,7 @@ class A2Lidar(Laser):
     def __init__(self):
         Laser.__init__(self, self.SAMPLES_COUNT, self.SAMPLING_RATE_HZ, 360, self.HOLE_WIDTH, offset_mm=25)
         
+
 def convert_point(angle, distance, strength):
     angle_r = angle*math.pi/180.0
     x = distance*math.cos(angle_r)
@@ -45,36 +50,33 @@ def convert_point(angle, distance, strength):
 
 
 def read_scans(data_source):
+    global g_Slam
+    
     deg_per_scan = 360/A2Lidar.SAMPLES_COUNT
-
+    
     for scan in data_source:
+        if g_Quitting:
+            break
+        
         payload = scan['scan']
         points = (((360-p['angle'])%360, p['distance'], p['strength']) for p in payload)
         
         points = np.array(list(points))
-
-        #threshold = np.percentile(points[:, 2], 30)
-        # threshold = 0
-        # points = points[points[:, 2] > threshold]
         
         distance_bins = [0] * A2Lidar.SAMPLES_COUNT
         
         for p in points:
 
             bin_no = int(p[0]/deg_per_scan)
-            try:
-                distance_bins[bin_no] = p[1] # Copy distance value at an angle to a resulting bin
-            except:
-                print(f"WTF {bin_no} - {p}")
+            distance_bins[bin_no] = p[1] # Copy distance value at an angle to a resulting bin
 
-        yield distance_bins
+        g_Slam['scan'] = distance_bins
 
 
 def render_map(screen, slam_map):
     screen.fill((0,0,0))
     
     map_ps = np.array(slam_map).reshape((MAP_SIZE_PIXELS, MAP_SIZE_PIXELS))
-    map_ps = np.rot90(map_ps, k=2)
     
     render_plane = imresize(map_ps, (SCREEN_SIZE_PIXELS, SCREEN_SIZE_PIXELS))
 
@@ -83,71 +85,103 @@ def render_map(screen, slam_map):
     pygame.surfarray.blit_array(screen, render_plane)
     
 
-def render_bot_reference(surface, x, y, theta):
+def render_bot_reference(surface):
+    x, y, theta = g_Slam['pos']
 
-    scale = (MAP_SIZE_PIXELS/(MAP_SIZE_METERS*1000))
-    arrow_length = 20 # arrow length in pixels
+    map_mm = MAP_SIZE_METERS*1000
     
+    scale = (MAP_SIZE_PIXELS/map_mm)
+    arrow_length = 20 # arrow length in pixels
+
+    x = x-(map_mm/2)
+    y = y-(map_mm/2)
+
     x = x*scale
     y = y*scale
 
-    sx = x*(SCREEN_SIZE_PIXELS/MAP_SIZE_PIXELS)
-    sy = y*(SCREEN_SIZE_PIXELS/MAP_SIZE_PIXELS)
+    sx = SCREEN_SIZE_PIXELS/2 + x*(SCREEN_SIZE_PIXELS/MAP_SIZE_PIXELS)
+    sy = SCREEN_SIZE_PIXELS/2 - y*(SCREEN_SIZE_PIXELS/MAP_SIZE_PIXELS)
     
     ex = sx+arrow_length*math.cos(theta)
     ey = sy+arrow_length*math.sin(theta)
 
-    pygame.draw.circle(surface, (0, 255, 0), (int(sx), int(sy)), int(arrow_length/2), 3)
-    pygame.draw.line(surface, (255, 0, 0), (sx, sy), (ex, ey))
+    pygame.draw.circle(surface, (0, 255, 0), (int(sy), int(sx)), int(arrow_length/2), 3)
+    pygame.draw.line(surface, (255, 0, 0), (sy, sx), (ey, ex))
 
+
+def do_the_slam_thing():
+    global g_Slam
+
+    reads_count = 0
+    x, y = 0, 0
+
+    slam = g_Slam['core']
     
-def do_the_slam_thing(slam, mapbytes, data_source):
+    while not g_Quitting:
+        if not g_Slam['scan']:
+            continue
+        
+        slam.update(g_Slam['scan'], (0, 0, 0))
+        g_Slam['pos'] = slam.getpos()
+        
+        slam.getmap(g_Slam['map'])
+        time.sleep(0.1)
+            
+
+def start_slam():
+    global g_Slam
+
+    lidar = A2Lidar()
+    g_Slam['core'] = RMHC_SLAM(lidar, MAP_SIZE_PIXELS, MAP_SIZE_METERS)
+    t = Thread(target=do_the_slam_thing)
+    t.start()
+
+
+def start_scanning(cfg):
+    scan_stream = SubSocket(cfg)
+    t = Thread(target=read_scans, args=(scan_stream,))
+    t.start()
+    
+    
+def render_loop():
+    global g_Quitting
     screen = pygame.display.set_mode((SCREEN_SIZE_PIXELS, SCREEN_SIZE_PIXELS), pygame.DOUBLEBUF)
 
     reads_count = 0
     x, y = 0, 0
 
     while True:
-    for scan in data_source:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
 
-        slam.update(scan, (0, 0, 0))
-        x, y, theta = slam.getpos()
-        slam.getmap(mapbytes)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                g_Quitting = True
+                return
+
+        mapbytes = g_Slam['map']
 
         render_map(screen, mapbytes)
-        render_bot_reference(screen, x, y, theta)
+        render_bot_reference(screen)
         
         pygame.display.flip()
-
-        reads_count += 1
-        if reads_count % SCANS_THRESHOLD == 0:
-            print("... {} scans, est'd position: {}, {}, angle {}".format(reads_count, x, y, theta))
-            with open("map_updates/map_{}".format(reads_count), "wb") as f:
-                f.write(mapbytes)
-            
-
+    
+                
 def main():
     print("Initializing lidar server")
-    pygame.init()
         
     with open("./config.yaml", "rt") as f:
         cfg = yaml.load(f)
 
-    socket = SubSocket(cfg)
-    lidar = A2Lidar()
-
     print("Initializing SLAM")
 
-    mapbytes = bytearray(MAP_SIZE_PIXELS**2)
-    slam = RMHC_SLAM(lidar, MAP_SIZE_PIXELS, MAP_SIZE_METERS)
+    g_Slam['map'] = bytearray(MAP_SIZE_PIXELS**2)
 
-    print("We're set for liftoff. reading")
+    pygame.init()
 
-    do_the_slam_thing(slam, mapbytes, read_scans(socket))
+
+    start_scanning(cfg)
+    start_slam()
     
-            
+    render_loop()
                        
                        
     
